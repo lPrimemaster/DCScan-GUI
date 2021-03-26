9c33ed759e08b5b6d5a8e739a53beb3fb6d80c02
César Godinho <cesarsky5@gmail.com>
2021-03-26 17:33:52 +0000

    Implement app testing.

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -3,6 +3,7 @@
 
 #include <fstream>
 #include <iterator>
+#include <sstream>
 
 #include <QApplication>
 #include <QDebug>
@@ -13,42 +14,46 @@
 using namespace DCS::Utils;
 using namespace DCS::Network;
 
-static std::vector<float> RunToWaitPoll(int rep, float acc_min = 1.0f, float acc_max = 200.0f)
+static std::vector<std::vector<float>> RunToWaitPoll(int rep, float acc_min = 1.0f, float acc_max = 200.0f)
 {
 	LOG_DEBUG("Starting RotDev test battery...");
 
 	unsigned char buffer[1024];
 	auto size_written = DCS::Registry::SVParams::GetDataFromParams(buffer,
-		SV_CALL_DCS_Control_IssueGenericCommand,
+		SV_CALL_DCS_Control_IssueGenericCommandResponse,
 		DCS::Control::UnitTarget::ESP301,
-		DCS::Utils::BasicString{ "2MO;2OR1;2WS;2AU200;" }
-		//DCS::Utils::BasicString{ "2MO;2PA0.0;" }
+		DCS::Utils::BasicString{ "2MO;2OR1;2WS;2AU2500;2VU40;2VA40;2AU?" }
 	);
-	Message::SendSync(Message::Operation::REQUEST, buffer, size_written);
+
+	auto zbv = Message::SendSync(Message::Operation::REQUEST, buffer, size_written);
+
+	auto zb_ret = *(DCS::Utils::BasicString*)zbv.ptr;
+	LOG_DEBUG("2AU: %s", zb_ret.buffer);
+
 
 	size_written = DCS::Registry::SVParams::GetDataFromParams(buffer,
 		SV_CALL_DCS_Control_IssueGenericCommandResponse,
 		DCS::Control::UnitTarget::ESP301,
-		DCS::Utils::BasicString{ "2ZH?;" }
+		DCS::Utils::BasicString{ "2ZB5H;WT1000;2ZB?" }
 	);
-	auto ret = Message::SendSync(Message::Operation::REQUEST, buffer, size_written);
-	LOG_DEBUG("ZH: %s", (*(DCS::Utils::BasicString*)ret.ptr));
 
-	size_written = DCS::Registry::SVParams::GetDataFromParams(buffer,
-		SV_CALL_DCS_Control_IssueGenericCommandResponse,
-		DCS::Control::UnitTarget::ESP301,
-		DCS::Utils::BasicString{ "2ZS?;" }
-	);
-	ret = Message::SendSync(Message::Operation::REQUEST, buffer, size_written);
-	LOG_DEBUG("ZS: %s", (*(DCS::Utils::BasicString*)ret.ptr));
+	auto wfm = Message::SendSync(Message::Operation::REQUEST, buffer, size_written);
+	auto wfm_ret = *(DCS::Utils::BasicString*)wfm.ptr;
+	LOG_DEBUG("2ZB: %s", wfm_ret.buffer);
 
-	std::vector<float> data;
+	LOG_DEBUG("Finished waiting for homing");
+
+	std::vector<std::vector<float>> data = { std::vector<float>(),
+											 std::vector<float>(),
+											 std::vector<float>(),
+											 std::vector<float>(),
+											 std::vector<float>() };
 	float acc_step = (acc_max - acc_min) / rep;
 
 	for (int i = 0; i < rep; i++)
 	{
 		float acc = acc_min + acc_step * i;
-		std::string acc_str = "2OR0;2WS;2AC" + std::to_string(acc) + ";2PA45;2WS;2PA0;2WS;2TP?;";
+		std::string acc_str = "2PA0;2WS;2AC" + std::to_string(acc) + ";2AG" + std::to_string(acc) + ";2PA45.010;2WS;2TP?";
 
 		DCS::Utils::BasicString reqstr;
 
@@ -59,11 +64,24 @@ static std::vector<float> RunToWaitPoll(int rep, float acc_min = 1.0f, float acc
 			DCS::Control::UnitTarget::ESP301,
 			reqstr
 		);
-		auto ret = Message::SendSync(Message::Operation::REQUEST, buffer, size_written);
-		float rpos = atof((*(DCS::Utils::BasicString*)ret.ptr).buffer);
-		LOG_DEBUG("Acc: %f || Dev: %f", acc, rpos);
-		data.push_back(rpos);
+
+		for (int j = 0; j < 100000; j++)
+		{
+			auto ret = Message::SendSync(Message::Operation::REQUEST, buffer, size_written);
+			float rpos = atof((*(DCS::Utils::BasicString*)ret.ptr).buffer);
+			LOG_DEBUG("Acc: %f || Dev: %f", acc, rpos);
+			data.at(i).push_back(rpos);
+		}
 	}
+
+	size_written = DCS::Registry::SVParams::GetDataFromParams(buffer,
+		SV_CALL_DCS_Control_IssueGenericCommand,
+		DCS::Control::UnitTarget::ESP301,
+		DCS::Utils::BasicString{ "2MF" }
+		//DCS::Utils::BasicString{ "2MO;2PA0.0;" }
+	);
+	Message::SendSync(Message::Operation::REQUEST, buffer, size_written);
+
 	return data;
 }
 
@@ -77,7 +95,7 @@ int main(int argc, char *argv[])
 
 	Client::StartThread(c);
 
-	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 
 	unsigned char buffer[1024];
 	auto size_written = DCS::Registry::SVParams::GetDataFromParams(buffer,
@@ -89,18 +107,21 @@ int main(int argc, char *argv[])
 
 	LOG_DEBUG("Got server max thread concurrency: %d", max_threads_srv);
 
-	size_written = DCS::Registry::SetupEvent(buffer, SV_EVT_OnTestFibSeq, [] (DCS::u8* data) {
+	/*size_written = DCS::Registry::SetupEvent(buffer, SV_EVT_OnTestFibSeq, [] (DCS::u8* data) {
 		LOG_DEBUG("FibEvent returned: %llu", *(DCS::u64*)data);
 	});
 
-	Message::SendAsync(Message::Operation::EVT_SUB, buffer, size_written);
+	Message::SendAsync(Message::Operation::EVT_SUB, buffer, size_written);*/
 
-	auto data = RunToWaitPoll(100);
+	auto data = RunToWaitPoll(5, 2500.0f, 20000.0f);
 
-	std::ofstream output_file("1_200ACC_S0_M45_S0_ABS.txt");
-	std::ostream_iterator<float> output_iterator(output_file, "\n");
-	std::copy(data.begin(), data.end(), output_iterator);
-
+	for (int j = 0; j < data.size(); j++)
+	{
+		std::ofstream output_file(std::to_string(j) + "_ACC_360_DEV.dat");
+		std::ostream_iterator<float> output_iterator(output_file, "\n");
+		std::copy(data[j].begin(), data[j].end(), output_iterator);
+	}
+	
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
 	Client::StopThread(c);
