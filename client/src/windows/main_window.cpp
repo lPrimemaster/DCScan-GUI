b8fd6eba8c2d3c902764401c28b3cdf3ec80a6a5
lPrimemaster <cesarsky5@gmail.com>
2023-05-22 10:28:55 +0100

    Add simulation window handler. Fix code indentation.

--- client/src/windows/main_window.cpp
+++ client/src/windows/main_window.cpp
@@ -25,6 +25,7 @@
 #include "mcaspectrum_window.h"
 #include "acquisitioncontrol_window.h"
 #include "clinometer_window.h"
+#include "simulation_window.h"
 
 #include <Windows.h>
 
@@ -205,49 +206,53 @@ MainWindow::MainWindow(QApplication* app, QWidget *parent) : QMainWindow(parent)
 
 	// Construct all windows
 	// And add them to the default view
-	auto log_window  	  	   = new LogWindow(this);
-	auto log_window_icon  	   = QIcon(":/png/log_window.png");
+	auto log_window  	  	               = new LogWindow(this);
+	auto log_window_icon  	               = QIcon(":/png/log_window.png");
 	AddGenericWindow("Log"			 , log_window	   , log_window_icon      , "View", ads::BottomDockWidgetArea);
 
-	auto hist_window 	  	   = new HistWindow(this);
-	auto hist_window_icon  	   = QIcon(":/png/spectrum_window.png");
+	auto hist_window 	  	               = new HistWindow(this);
+	auto hist_window_icon  	               = QIcon(":/png/spectrum_window.png");
 	AddGenericWindow("Spectrum Graph", hist_window     , hist_window_icon     , "View", ads::TopDockWidgetArea);
 
-	auto test_window           = new TestWindow(this);
-	auto test_window_icon      = QIcon(":/png/debug_window.png");
+	auto test_window                       = new TestWindow(this);
+	auto test_window_icon                  = QIcon(":/png/debug_window.png");
 	AddGenericWindow("Debug Window"  , test_window     , test_window_icon     , "View", ads::LeftDockWidgetArea);
 
-	auto remote_window         = new ConnectWindow(this);
-	auto remote_window_icon    = QIcon(":/png/remote_cont_window.png");
+	auto remote_window                     = new ConnectWindow(this);
+	auto remote_window_icon                = QIcon(":/png/remote_cont_window.png");
 	AddGenericWindow("Remote Control", remote_window   , remote_window_icon   , "View", ads::LeftDockWidgetArea);
 
-	auto freejog_window         = new FreejogWindow(this);
-	auto freejog_window_icon    = QIcon(":/png/freejog_window.png");
+	auto freejog_window                    = new FreejogWindow(this);
+	auto freejog_window_icon               = QIcon(":/png/freejog_window.png");
 	AddGenericWindow("Free Jog Control", freejog_window , freejog_window_icon , "View", ads::LeftDockWidgetArea);
 
-	auto position_window         = new PositionWindow(this);
-	auto position_window_icon    = QIcon(":/png/position_window.png");
+	auto position_window                   = new PositionWindow(this);
+	auto position_window_icon              = QIcon(":/png/position_window.png");
 	AddGenericWindow("Position Monitoring", position_window , position_window_icon , "View", ads::LeftDockWidgetArea);
 
-	auto channelviz_window         = new ChannelvizWindow(this);
-	auto channelviz_window_icon    = QIcon(":/png/channelviz_window.png");
+	auto channelviz_window                 = new ChannelvizWindow(this);
+	auto channelviz_window_icon            = QIcon(":/png/channelviz_window.png");
 	AddGenericWindow("Channel Visualizer", channelviz_window , channelviz_window_icon , "View", ads::NoDockWidgetArea);
 
-	auto spectraldisplay_window         = new SpectralDisplayWindow(this);
-	auto spectraldisplay_window_icon    = QIcon(":/png/spectraldisplay_window.png");
+	auto spectraldisplay_window            = new SpectralDisplayWindow(this);
+	auto spectraldisplay_window_icon       = QIcon(":/png/spectraldisplay_window.png");
 	AddGenericWindow("Acquisition Details", spectraldisplay_window , spectraldisplay_window_icon , "View", ads::NoDockWidgetArea);
 
 	auto acquisitioncontrol_window         = new AcquisitionControlWindow(this);
 	auto acquisitioncontrol_window_icon    = QIcon(":/png/acquisitioncontrol_window.png");
 	AddGenericWindow("Acquisition Control", acquisitioncontrol_window , acquisitioncontrol_window_icon , "View", ads::NoDockWidgetArea);
 
-	auto mcaspectrum_window         = new MCASpectrumWindow(this);
-	auto mcaspectrum_window_icon    = QIcon(":/png/mcaspectrum_window.png");
+	auto mcaspectrum_window                = new MCASpectrumWindow(this);
+	auto mcaspectrum_window_icon           = QIcon(":/png/mcaspectrum_window.png");
 	AddGenericWindow("MCA Spectrum", mcaspectrum_window , mcaspectrum_window_icon , "View", ads::NoDockWidgetArea);
 
-	auto clinometer_window         = new ClinometerWindow(this);
-	auto clinometer_window_icon    = QIcon(":/png/clinometer_window.png"); // TODO: arranjar um ícone para a janela
+	auto clinometer_window                 = new ClinometerWindow(this);
+	auto clinometer_window_icon            = QIcon(":/png/clinometer_window.png");
 	AddGenericWindow("Clinometer Monitoring", clinometer_window , clinometer_window_icon , "View", ads::NoDockWidgetArea);
+
+	auto simulation_window                 = new SimulationWindow(this);
+	auto simulation_window_icon            = QIcon(":/png/simulation_window.png");
+	AddGenericWindow("Simulation", simulation_window, simulation_window_icon, "View", ads::NoDockWidgetArea);
 
 	IssueStatusBarText("Loading perspectives...");
 
