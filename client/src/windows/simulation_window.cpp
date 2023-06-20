8e59b48852c9d48908536d6f00ae358957bf4c86
lPrimemaster <cesarsky5@gmail.com>
2023-06-20 10:59:04 +0200

    Display warning on simulation window unavailable.

--- client/src/windows/simulation_window.cpp
+++ client/src/windows/simulation_window.cpp
@@ -1,14 +1,40 @@
 #include "simulation_window.h"
 #include "./ui_simulation_window.h"
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 
-SimulationWindow::SimulationWindow(QWidget* parent) : ui(new Ui::SimulationWindow), settings_tab(this)
+SimulationWindow::SimulationWindow(QWidget* parent) : ui(new Ui::SimulationWindow)
+#ifdef DCSSIMU_FOUND
+	, settings_tab(this)
+#endif
 {
+#ifdef DCSSIMU_FOUND
 	ui->setupUi(this);
     ui->tabWidget->insertTab(0, &settings_tab, "Simulation Settings");
 	ui->tabWidget->insertTab(1, settings_tab.getSimulationWindow(), "Simulation Plots");
+#else
+	QLabel* message = new QLabel("Simulation package is missing.\nPlease install it from the DCSimu repository.", this);
+	QLabel* icon = new QLabel(this);
+	icon->setPixmap(QPixmap(":/png/warning.png"));
+	icon->setEnabled(false);
+	QVBoxLayout* window_layout = new QVBoxLayout(this);
+	message_layout = new QHBoxLayout();
+	message_layout->addItem(new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum));
+	message_layout->addWidget(icon);
+	QFont font;
+	font.setPointSize(12);
+	message->setFont(font);
+	message->setEnabled(false);
+	message_layout->addWidget(message);
+	message_layout->addItem(new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum));
+	window_layout->addLayout(message_layout);
+#endif
 }
 
 SimulationWindow::~SimulationWindow()
 {
+#ifndef DCSSIMU_FOUND
+	delete message_layout;
+#endif
 	delete ui;
 }
--- client/src/windows/simulation_window.h
+++ client/src/windows/simulation_window.h
@@ -4,9 +4,15 @@
 
 #include <QWidget>
 #include <QTabWidget>
+#include <QLabel>
+
+#ifdef DCSSIMU_FOUND
 #include <DCSsimu/dcs_gui_new.h>
+#endif
 
 UI_AUTOGEN(SimulationWindow)
+
+class QHBoxLayout;
 
 class SimulationWindow : public QWidget
 {
@@ -17,5 +23,9 @@ public:
 
 private:
     Ui::SimulationWindow* ui;
+#ifdef DCSSIMU_FOUND
     GUISettingsWindow settings_tab;
+#else
+	QHBoxLayout* message_layout;
+#endif
 };
